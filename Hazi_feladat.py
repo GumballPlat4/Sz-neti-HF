7f5664c59755989ff2d86f6415ced6b3956b2e43
GumballPlat4 <162676451+GumballPlat4@users.noreply.github.com>
2024-03-27 10:33:05 +0100

    Update Hazi_feladat.py

--- Hazi_feladat.py
+++ Hazi_feladat.py
@@ -1,6 +1,6 @@
 def beolvasas():
     lista=[]
-    with open ("./adatok/liba_adatok.txt","r",encoding="UTF-8") as fm:
+    with open ("adatok.txt","r",encoding="UTF-8") as fm:
         for sor in fm:
             lista.append(int(sor.strip()))
     return lista
